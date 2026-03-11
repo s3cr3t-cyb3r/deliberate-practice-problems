5d4dc5f0254f059b9120dbb8cebce38e5d6a6847
s3cr3t-cyb3r <lancesanchez2010@gmail.com>
2026-03-11 14:45:44 +1000

    Create practice.rb

---
+++ practice.rb
@@ -0,0 +1,35 @@
+# ALL BASIC PRACTICE PROBLEMS
+
+# Variables
+    # Question 1
+        num = rand(1..100)
+        puts num + 15
+    # Question 2
+        word = "hello"
+        puts word.upcase
+    # Question 3
+        x = rand(1..100)
+        y = rand(1..100)
+        puts x * y
+    # Question 4
+        palindrome = "racecar"
+        puts palindrome.reverse
+    # Question 5
+        number = rand(1..100)
+        puts number / 5
+    # Question 6
+        word1 = "hello"
+        word2 = "world"
+        puts = word1.upcase + " " + word2.upcase
+    # Question 7
+        long_word = "pneumonoultramicroscopicsilicovolcanoconiosis"
+        puts long_word.length
+    # Question 8
+        new_num = rand(1.0..100.0)
+        puts new_num.to_i
+    # Question 9
+        new_number = rand(1..100)
+        puts new_number ** 2
+    # Question 10
+        not_mad = "NOT ANGRY"
+        puts not_mad.downcase
